684bea37f5dc93d50a2a280bc7cbcde2393b674c
Thaihamster <asadanaohisa8215@gmail.com>
2019-10-28 16:45:32 +0700

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+#ページごとの完全なタイトル設定
+  def full_title(page_title = '')
+    base_title = "Reviewees"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Reviewees"
+    assert_select "title", "Reviewees"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Reviewees"
+    assert_select "title", "Reviewees"
   end
 
   test "should get privacy" do
     get static_pages_privacy_url
     assert_response :success
-    assert_select "title", "Privacy | Reviewees"
+    assert_select "title", "Reviewees"
   end
 
 end
