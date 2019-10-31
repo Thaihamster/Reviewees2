4fdcc83b8215c79048e17b8863e197644e155640
Thaihamster <asadanaohisa8215@gmail.com>
2019-10-31 09:39:17 +0700

    Finish user signup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Reviewees"
+    assert_select "title", "ホーム | Reviewees"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "Reviewees"
+    assert_select "title", "Revieweesについて | Reviewees"
   end
 
   test "should get privacy" do
     get privacy_path
     assert_response :success
-    assert_select "title", "Reviewees"
+    assert_select "title", "プライバシーポリシー | Reviewees"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Reviewees"
+    assert_select "title", "contact | Reviewees"
   end
 
 end
